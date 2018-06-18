5fea6f4548f5e64e42418de7fb4b88bf22a1ff5c
maayans <maayansavir@gmail.com>
2018-06-18 18:47:59 +0300

    remove en neccesery file

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import { connect } from 'react-redux';
-// style
-import './App.css';
 // components
 import SearchBar from '../SearchBar/SearchBar';
 import Weather from '../Data/Weather';
